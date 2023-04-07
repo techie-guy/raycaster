4cf5952b7a488446dd6146257c14edfce2fbdb31
techie-guy <devtechieguy@gmail.com>
2023-04-07 19:44:12 +0530

    Request OpenGL Core Profile

--- src/Window.c
+++ src/Window.c
@@ -11,6 +11,10 @@ void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id, GLen
 void initWindow(Window* window)
 {
 	glfwInit();
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window->handle = glfwCreateWindow(window->width, window->height, window->title, NULL, NULL);
 	if(!window->handle)
